ef271d34d33e747d874030a8db4184e82897bbca
Sajal Rastogi <30717607+sajalrasto@users.noreply.github.com>
2025-04-09 16:48:32 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,47 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "onfinance-ai"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones" {
+  description = "AWS availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "database_username" {
+  description = "RDS master username"
+  type        = string
+  sensitive   = true
+}
+
+variable "database_password" {
+  description = "RDS master password"
+  type        = string
+  sensitive   = true
+}
